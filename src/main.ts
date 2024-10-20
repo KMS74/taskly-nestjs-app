e2c40eea23c7588374aa794e864774b06fcd9b3b
Karem Shabana <km425360@gmail.com>
2024-10-20 21:11:02 +0300

    chore: add logger to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
-
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // global pipe to validate all incoming requests
 
   app.useGlobalInterceptors(new TransformInterceptor()); // global interceptor to transform all outgoing responses
-  await app.listen(3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
