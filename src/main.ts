3ca8a6dafd1e4bea054dd2032e1ccb645bfac70d
Karem Shabana <km425360@gmail.com>
2025-01-12 20:41:55 +0200

    feat: Enhance global validation pipe with whitelist and forbidNonWhitelisted options

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // global pipe to validate all incoming requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  ); // global pipe to validate all incoming requests
 
   app.useGlobalInterceptors(new TransformInterceptor()); // global interceptor to transform all outgoing responses
   const port = process.env.PORT ?? 3000;
