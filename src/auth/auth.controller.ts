a48a5125a6dd07d076206f1ca106007bf6628167
Karem Shabana <km425360@gmail.com>
2025-02-07 17:03:17 +0200

    feat: add get logged in user data api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,12 @@ export class AuthController {
   signin(@Body() authCredentialsDto: AuthCredentialsDto) {
     this.logger.log('User signing in...');
     return this.authService.signin(authCredentialsDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getProfile(@GetUser() user) {
+    this.logger.log('Getting user data...');
+    return user;
   }
 }
