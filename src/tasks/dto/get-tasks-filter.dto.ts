23aa47646347fd500c200da2b4fb9c65ef9ea611
Karem Shabana <km425360@gmail.com>
2025-04-03 20:36:34 +0200

    feat: add Swagger API properties to GetTasksFilterDto and UpdateTaskStatusDto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,12 +1,15 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
+import { ApiProperty } from '@nestjs/swagger';
 // Dto for query params for filtering tasks
 // ?status=OPEN&search=Some%20search%20query
 export class GetTasksFilterDto {
+  @ApiProperty()
   @IsOptional()
   @IsEnum(TaskStatus)
   status: TaskStatus;
 
+  @ApiProperty()
   @IsOptional()
   @IsNotEmpty()
   @IsString()
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,7 @@
 import { CreateTaskDto } from './create-task.dto';
 import { IsEnum } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
 import { TaskStatus } from '../task-status.enum';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 // The PartialType() function creates a new class that extends the original
 // UpdateTaskDto class with all the properties set to optional.
@@ -9,6 +9,7 @@ export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
 
 export class UpdateTaskStatusDto {
   // validate that the status is one of the values from the TaskStatus enum
+  @ApiProperty({ enum: TaskStatus })
   @IsEnum(TaskStatus)
   status: TaskStatus;
 }
